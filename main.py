083be0bfd55853730f4b0bf2856846e23f3443f0
Gomes Cabral <phgcabral@hotmail.com>
2024-01-06 17:50:21 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+class Bill:
+    """Object that contains data about a bill, such as totam amount and period of the bill"""
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+        
+
+class Flatmate:
+    """Create a flatmate person who lives in the flat and pays a share of the bill"""
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        pass
+
+
+class PdfReport:
+    """Creates a Pdf file thet contains data about the flatmates such as their names, their due amounts and the period of the bill"""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
